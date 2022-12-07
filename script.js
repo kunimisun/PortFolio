338641c40b484ab98943927b1ac494794597b637
kunimisun <83684034+kunimisun@users.noreply.github.com>
2022-12-07 19:43:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {
+  //id名を指定
+  easing: "easeInOut", //アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000, //時間指定(1000＝1秒)
+  strokeWidth: 0.2, //進捗ゲージの太さ
+  color: "#555", //進捗ゲージのカラー
+  trailWidth: 0.2, //ゲージベースの線の太さ
+  trailColor: "#bbb", //ゲージベースの線のカラー
+  text: {
+    //テキストの形状を直接指定
+    style: {
+      //天地中央に配置
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      padding: "0",
+      margin: "-30px 0 0 0", //バーより上に配置
+      transform: "translate(-50%,-50%)",
+      "font-size": "1rem",
+      color: "#fff",
+    },
+    autoStyleContainer: false, //自動付与のスタイルを切る
+  },
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + " %"); //テキストの数値
+  },
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {
+  //バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash_text").fadeOut(10); //フェードアウトでローディングテキストを削除
+  $(".loader_cover-up").addClass("coveranime"); //カバーが上に上がるクラス追加
+  $(".loader_cover-down").addClass("coveranime"); //カバーが下に下がるクラス追加
+  $("#splash").fadeOut(); //#splashエリアをフェードアウト
+});
+
+$(".openbtn1").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
+  $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+});
+
+// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
+function TextTypingAnime() {
+  $(".TextTyping").each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var thisChild = "";
+    if (scroll >= elemPos - windowHeight) {
+      thisChild = $(this).children(); //spanタグを取得
+      //spanタグの要素の１つ１つ処理を追加
+      thisChild.each(function (i) {
+        var time = 100;
+        //時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
+        $(this)
+          .delay(time * i)
+          .fadeIn(time);
+      });
+    } else {
+      thisChild = $(this).children();
+      thisChild.each(function () {
+        $(this).stop(); //delay処理を止める
+        $(this).css("display", "none"); //spanタグ非表示
+      });
+    }
+  });
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  TextTypingAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  //spanタグを追加する
+  var element = $(".TextTyping");
+  element.each(function () {
+    var text = $(this).html();
+    var textbox = "";
+    text.split("").forEach(function (t) {
+      if (t !== " ") {
+        textbox += "<span>" + t + "</span>";
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  TextTypingAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+//線が伸びるための設定を関数でまとめる
+function ScrollTimelineAnime() {
+  $(".timeline li").each(function () {
+    // それぞれのli要素の
+    var elemPos = $(this).offset().top; // 上からの高さ取得
+    var scroll = $(window).scrollTop(); // スクロール値取得
+    var windowHeight = $(window).height(); // windowの高さ取得
+    var startPoint = 100; //線をスタートさせる位置を指定※レイアウトによって調整してください
+    if (scroll >= elemPos - windowHeight - startPoint) {
+      var H = $(this).outerHeight(true); //liの余白と高さを含めた数値を取得
+      //スクロール値から要素までの高さを引いた値を、liの高さの半分のパーセントで出す
+      var percent = ((scroll + startPoint - elemPos) / (H / 2)) * 100; //liの余白と高さの半分で線を100％に伸ばす
+
+      // 100% を超えたらずっと100%を入れ続ける
+      if (percent > 100) {
+        percent = 100;
+      }
+      // ボーダーの長さをセット
+      $(this)
+        .children(".border-line")
+        .css({
+          height: percent + "%", //CSSでパーセント指定
+        });
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).on("scroll", function () {
+  ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
+});
